8e7fff6202a6bbff80375170ee56736889de08ff
Cristiano <cristianochincarosanches@gmail.com>
2025-06-03 20:42:44 -0300

    remove_porcentage_in_findout_numbers

--- findout_number/findout_number.py
+++ findout_number/findout_number.py
@@ -8,54 +8,51 @@ def generate_number()->int:
     chose_dir=(dirs_home[int(random.random()*len(dirs_home))])
     # print(chose_dir)
     return len(chose_dir)
-def view_list(list_numbers:"list",*args):
-    amount_items=0
-    if len(args):
-        amount_items=args[0]
-    else:
-        amount_items=len(list_numbers)
-    #
-    if amount_items>len(list_numbers):
-        amount_items=len(list_numbers)
-    #
-    sorted_list=sorted(list_numbers,key=lambda c:list(c.values())[0])
-    sorted_list.reverse()
-    print("number \t distance")
-    for i in range(amount_items):
-        print("{} \t {:.10f}".format(list(sorted_list[i].keys())[0],list(sorted_list[i].values())[0]))
 
+def main_findout_number():
+	print("\n")
+	while True:
+		random_num=int(generate_number()*random.random()*400)
+		list_numbers=[{0:0}] # number X distance
+		steps=0
+		while True:
+			steps+=1
+			inp=input("*: ")
+			big_distance=list(list_numbers[0].values())[0]
+			big_distance_num=list(list_numbers[0].keys())[0]
+			if inp=="p":
+				print(big_distance)
+				continue
+			elif not inp[0].isdigit():
+				print("Invalid command")
+				continue
+			#
+			inp_int=int(inp)
+			distance=1-abs(inp_int-random_num)/random_num
+			distance*=100
+			#
+			if big_distance > distance:
+				print("{} is more distant that {}".format(inp_int,big_distance_num))
+			elif big_distance < distance:
+				print("{} is more close that {}".format(inp_int,big_distance_num))
+			else:
+				print("{} is in same distance that {}".format(inp_int,big_distance_num))
+			#
+			stop=0
+			for i in list_numbers:
+				if inp_int==list(i.keys())[0]:
+					stop=1
+					break
+			if stop:
+				continue
+			list_numbers.append({inp_int:distance})
+			temp=sorted(list_numbers,key=lambda c:list(c.values())[0])
+			temp.reverse()
+			list_numbers=temp
+			#
+			if distance == 100:
+				print("Congratulation!\nYou win the game with {} steps".format(steps))
+				break
 
-print("Hello!\nWelcome to findout number game!\nIn this game, your aim is discover the number by of information given by an entried number. Each number inputed, is print a float value. This means how much longer the entered number is of the random number in porcentage.\nThe distance is calculate through the operation: abs(inputed_number-random_number)/random_number")
-print("\n\n")
-commands="H-> display this menu\nv [amount_items]-> view the list of biggest porcentage\n"
-print(commands)
-while True:
-    random_num=int(generate_number()*(random.random()*400))
-    # print(random_num)
-    list_biggest=[]
-    try:
-        while True:
-            inp=input("* ").split()
-            if not len(inp):
-                continue
-            if inp[0] == "v" and len(inp)>1:
-                view_list(list_biggest,int(inp[1]))
-                continue
-            elif inp[0]=="v" and len(inp)<2:
-                view_list(list_biggest)
-                continue
-            elif not inp[0][0].isdigit:
-                print("Invalid command")
-                continue
-            #
-            inp_int=int(inp[0])
-            distance=1-abs(random_num-inp_int)/random_num
-            distance*=100
-
-            list_biggest.append({inp_int:distance})
-            print(distance)
-            if distance==100:
-                print("Congratulation!!!\nYou win after {} try".format(len(list_biggest)))
-                break
-    except KeyboardInterrupt:
-        break
+if __name__=="__main__":
+    main_findout_number()
