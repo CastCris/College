2211d26e9a5ad0c02ea11a9d99078ac42b68a293
Cristiano <cristianochincarosanches@gmail.com>
2025-07-16 21:14:13 -0300

    app_update--total_user_manager_feature_work--

--- app.py
+++ app.py
@@ -20,9 +20,9 @@ def before_request()->None:
     global user
     #
     user=None
-    if "USER" in flask.request.cookies:
+    if "user" in flask.request.cookies:
         user=User(flask.request.cookies.get("user"))
-        user.get_infos()
+        user.get_infos(DIVISOR_VAR_CONTENT)
         user.display_cli()
     define_users()
     define_notices()
@@ -51,9 +51,11 @@ def index():
 
 @app.route('/login_display')
 def login_display()->object:
-    return flask.render_template('account/login.html',user=user)
-@app.route('/login_authenticate',methods=['POST'])
-def login_authenticate()->object:
+    if not user:
+        return flask.render_template('account/login.html',user=user)
+    return flask.redirect(flask.url_for('index_display'))
+@app.route('/login_auth',methods=['POST'])
+def login_auth()->object:
     global user_control
     global user
     #
@@ -61,26 +63,23 @@ def login_authenticate()->object:
         username=flask.request.form['username']
         passphrase=flask.request.form['passphrase']
         #
-        user=user_control.get_user(username)
+        user=user_control.get_user(username,DIVISOR_VAR_CONTENT)
         if not user:
             flask.flash(f"The user {username} doesn't exist","error")
-            return flask.redirect(flask.url_for("login_display"))
-        user.get_infos(DIVISOR_VAR_CONTENT)
+            return flask.render_template("account/login.html",messages="The user {} doesn't exist".format(username))
         passphrase_user=(user.get_attr("passphrase"))[0]
         user.display_cli()
-        print(user.get_attr("path_infos"))
-        print(user.__dict__)
+        # print(user.get_attr("path_infos"))
+        # print(user.__dict__)
         #
         if passphrase==passphrase_user:
-            response=flask.make_response("Set cookie user")
-            response.set_cookie("USER",user.get_attr("path_infos")[0])
-            print(response.cookie.get("USER"))
-            return flask.redirect(flask.url_for('index_display'))
-        flask.flash("Deu ruim, faz de novo o login","error")
-        return flask.redirect(flask.url_for("login_display"))
+            response=flask.make_response(flask.redirect('/'))
+            response.set_cookie("user",user.get_attr("path_infos")[0])
+            return response
+        return flask.render_template("account/login.html",messages="The user pass is incorret".format(passphrase))
 @app.route('/sign_display')
 def sign_display()->object:
-    return flask.render_template('account/sign.html')
+    return flask.render_template('account/sign.html',user=user)
 @app.route('/create_account',methods=['POST'])
 def create_account()->object:
     global user_control
@@ -90,7 +89,7 @@ def create_account()->object:
         user_pass=flask.request.form['user_passphrase']
         user_plus=flask.request.form['plus_infos']
         if user_control.get_item(user_name):
-            return flask.render_template('sign.html',message="This user already exist, create other")
+            return flask.render_template('sign.html',message="This user already exist, create other",user=user)
         #
         attr={}
         attr["NAME"]=[user_name]
@@ -102,7 +101,7 @@ def create_account()->object:
         return flask.redirect(flask.url_for('login_display'))
 @app.route('/change_pass_display')
 def change_pass_display()->object:
-    return flask.render_template('account/change_passphrase.html')
+    return flask.render_template('account/change_passphrase.html',user=user)
 @app.route('/change_pass_auth',methods=['POST'])
 def change_pass_auth()->object:
     global user_control
@@ -114,36 +113,36 @@ def change_pass_auth()->object:
         user_pass_new=flask.request.form['user_pass_new']
         #
         if not user_control.get_item(user_name):
-            return flask.render_template('account/change_pass.html',message="This user doesn't exist, create one")
-        user=user_control.get_user(user_name)
-        user.get_infos(DIVISOR_VAR_CONTENT)
+            return flask.render_template('account/change_pass.html',message="This user doesn't exist, create one",user=user)
+        user=user_control.get_user(user_name,DIVISOR_VAR_CONTENT)
         user.display_cli()
         if user_pass==user.get_attr("passphrase")[0]:
             user_control.update_item(user_name,"passphrase",user_pass_new,DIVISOR_VAR_CONTENT)
             return flask.redirect(flask.url_for('login_display'))
-        return flask.render_template("account/change_pass.html",message="Invalid passphrase")
+        return flask.render_template("account/change_pass.html",message="Invalid passphrase",user=user)
 @app.route("/logout")
 def logout()->object:
-    flask.request.set_cookie("user",'',expires=0)
-    user=None
+    response=flask.make_response(flask.redirect("/"))
+    response.set_cookie('user','',expires=0)
 
-    return flask.redirect(flask.url_for('index'))
+    return response
 
 @app.route('/index_display')
 def index_display()->object:
     global notice_control
     #
     notices=notice_control.get_notices(DIVISOR_VAR_CONTENT)
-    return flask.render_template('index.html',search='*',user=user)
-@app.route('/home_page_search',methods=['POST'])
-def home_page_search()->None:
-    global notices
+    return flask.render_template('index.html',notices=notices,user=user)
+@app.route('/index_search',methods=['GET'])
+def index_search()->None:
+    global notice_control
     #
-    if flask.request.method=='POST':
-        pattern=flask.request.form['pattern'].lower()
+    if flask.request.method=='GET':
+        pattern=flask.request.args['pattern'].lower()
+        notices=notice_control.get_notices_filter(pattern,DIVISOR_VAR_CONTENT)
         for i in notices:
-            search_result=match_item(i.type,pattern)
-        return flask.render_template('index.html',notices=notices,search=pattern)
+            i.display_cli()
+        return flask.render_template('index.html',notices=notices,user=user)
 
 if __name__=='__main__':
     app.run(debug=True)
