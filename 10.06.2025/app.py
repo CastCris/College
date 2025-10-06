bbac6d9320a3ca709b4d56f0e4b24b9655d9a3ba
Cristiano <cristianochincarosanches@gmail.com>
2025-10-06 17:49:00 -0300

    build_basic_code_for_activity_of_10.06.2025_day

--- 10.06.2025/app.py
+++ 10.06.2025/app.py
@@ -1,6 +1,8 @@
 import flask
+import math
 
 app = flask.Flask(__name__)
+app.config["SECRET_KEY"] = '123445678'
 
 products = [
         {"id": 1, "name": "Notebook Gamer X", "price": 5200.00},
@@ -12,7 +14,33 @@ products = [
 
 @app.route('/')
 def index()->object:
-    return '<h1>Hello World!</h1>'
+    flask.session["products"] = products
+
+    return flask.redirect('/products_by_page/1');
+
+@app.route('/products_by_page/<index>')
+def product_list_by_page(index)->object:
+    page = int(index)
+    per_page = 5
+
+    #
+    start = (page-1) * per_page
+    end = start + per_page
+
+    total_pages = math.cell(len(products) / per_page)
+
+    products_page = products[start:end]
+
+    print(products_page)
+
+    return 'AAAAA'
+    """
+    #
+    flask.session["page_start"] = start
+    flask.session["page_end"] = end
+
+    return flask.render_template('products_by_page')
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
