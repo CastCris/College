48a01d5a9ff273be4a3a855ddf16e832046d39de
Cristiano <cristianochincarosanches@gmail.com>
2025-12-18 21:57:45 -0300

    update form erros display function

--- 2_year/HappyRotel/app/begin/globals/Forms.py
+++ 2_year/HappyRotel/app/begin/globals/Forms.py
@@ -1,7 +1,7 @@
 filter_str = lambda value: value.strip() if value else None
 
 forms_errors = lambda *forms: [
-    ': '.join([ tag, ', '.join(value) ])
-    for i in range(len(forms))
-    for tag, value in forms[i].errors.items() 
+        ': '.join([ form[tag].label.text, ', '.join(errors)])
+    for form in forms
+    for tag, errors in form.errors.items()
 ]
