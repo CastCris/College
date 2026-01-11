e09dac50df19e2b9e5fc004d4bde451d667ab55d
Cristiano <cristianochincarosanches@gmail.com>
2026-01-11 18:01:42 -0300

    update ManagerUser and Role

--- 2_year/HappyRotel/app/begin/globals/flask_auth/Manager.py
+++ 2_year/HappyRotel/app/begin/globals/flask_auth/Manager.py
@@ -2,6 +2,10 @@ from begin.globals import Crypt, Class
 
 from sqlalchemy.orm import DeclarativeMeta
 from functools import wraps
+
+import argon2
+import secrets
+import string
 import redis
 import os
 
@@ -10,37 +14,33 @@ from .UserAuth import UserAuth
 
 ##
 class MU_TokenService():
-    MU_TOKEN_VALIDITY = 60* 60 * 24 * 7
-    MU_TOKEN_AUTH = Crypt.argon2_auth
-
-    MU_TOKEN_TYPES = ['Redis', 'FileSystem']
-    MU_TOKEN_TYPE = 'Redis'
-    MU_TOKEN_REDIS = redis.Redis()
+    _MU_SERVICE_TYPE = ['Redis', 'FileSystem']
+    MU_SERVICE_TYPE = 'Redis'
+    MU_SERVICE_REDIS = redis.Redis()
 
     ## Token redis manipulation
     @classmethod
     def token_save(cls, key:str, token:str)->None:
-        r = cls.MU_TOKEN_REDIS
+        r = cls.MU_SERVICE_REDIS
         print('redis: ', r, key, token)
 
         r.set(key, token)
-        r.expire(key, cls.MU_TOKEN_VALIDITY)
+        r.expire(key, MU_Token.MU_TOKEN_VALIDITY)
 
     @classmethod
     def token_delete(cls, key:str)->None:
-        r = cls.MU_TOKEN_REDIS
+        r = cls.MU_SERVICE_REDIS
         r.delete(key)
 
     @classmethod
     def token_get(cls, key:str)->str | None:
-        r = cls.MU_TOKEN_REDIS
+        r = cls.MU_SERVICE_REDIS
         return r.get(key)
 
     @classmethod
-    def token_auth(cls, key:str=None, token:str=None)->bool:
+    def token_validate(cls, key:str=None, token:str=None)->bool:
         token_hashed = cls.token_get(key)
-        print('token_hashed: ', token_hashed, token)
-        return cls.MU_TOKEN_AUTH(token_hashed, token)
+        return MU_Token._MU_TOKEN_AUTH(token_hashed, token)
         
 
 class MU_Client():
@@ -61,8 +61,8 @@ class MU_Client():
         pass
 
     ##
-    @Class.property
-    def token_auth(cls)->str|None:
+    @classmethod
+    def token_auth_get(cls)->str|None:
         return cls.cookie_get(cls.MU_CLIENT_TOKEN_NAME)
 
     @classmethod
@@ -83,13 +83,13 @@ class MU_Client():
         cls.cookie_def(cls.MU_CLIENT_USER_NAME, user)
 
     @classmethod
-    def user_delete(cls, key:str)->None:
+    def user_delete(cls)->None:
         cls.user_define(None)
 
     ##
     @Class.property
     def logged(cls)->bool:
-        return cls.token_auth and MU_TokenService.token_auth(**cls.token_auth)
+        return cls.token_auth_get() and MU_TokenService.token_validate(**cls.token_auth_get())
 
     @Class.property
     def logoutted(cls)->bool:
@@ -136,34 +136,54 @@ class MU_Interface():
 
 
 class MU_Token():
-    MU_KEY_PREFIX = "token:auth"
-    MU_KEY_FUNC = Crypt.code_generate
-    MU_KEY_GENERATE = classmethod(lambda cls: cls.MU_KEY_PREFIX + ':' + cls.MU_KEY_FUNC())
+    MU_KEY_PREFIX = "mu:token:auth"
+    _MU_KEY_FUNC = lambda : ''.join([ secrets.choice(string.ascii_letters + string.digits) for _ in range(32)])
+    _MU_KEY_GENERATE = lambda : MU_Token.MU_KEY_PREFIX + ':' + MU_Token._MU_KEY_FUNC()
 
-    MU_TOKEN_CRYPT = Crypt.argon2_crypt
-    MU_TOKEN_FUNC = Crypt.code_generate
-    MU_TOKEN_GENERATE = classmethod(lambda cls: cls.MU_TOKEN_FUNC())
+    _MU_TOKEN_CRYPT = lambda value: argon2.PasswordHasher().hash(value)
+    _MU_TOKEN_FUNC = lambda : ''.join([ secrets.choice(string.ascii_letters + string.digits) for _ in range(32)])
+    _MU_TOKEN_GENERATE = lambda : MU_Token._MU_TOKEN_FUNC()
+
+    MU_TOKEN_VALIDITY = 60* 60 * 24 * 7
+    _MU_TOKEN_AUTH = lambda value_hashed, value_input: argon2.PasswordHasher().verify(value_hashed, value_input)
     
     ##
-    KEY:str = None
-    TOKEN:str = None
+    _KEY:str = None
+    _TOKEN:str = None
 
     def __init__(self, key:str=None, token:str=None)->None:
-        self.KEY = key or self.__class__.MU_KEY_GENERATE()
-        self.TOKEN = token or  self.__class__.MU_TOKEN_GENERATE()
+        self._KEY = key or self.__class__._MU_KEY_GENERATE()
+        self._TOKEN = token or  self.__class__._MU_TOKEN_GENERATE()
+    
+    ## Decorators
+    @staticmethod
+    def tokenKey_gen(func)->None:
+        MU_Token._MU_KEY_FUNC = func
 
-    ##
+    @staticmethod
+    def token_gen(func)->None:
+        MU_Token._MU_TOKEN_FUNC = func
+
+    @staticmethod
+    def token_crypt(func)->None:
+        MU_Token._MU_TOKEN_CRYPT = func
+
+    @staticmethod
+    def token_verify(func)->None:
+        MU_Token._MU_TOKEN_AUTH = func
+
+    ## Porpertys
     @property
     def key(self)->str:
-        return self.KEY
+        return self._KEY
 
     @property
     def token(self)->str:
-        return self.TOKEN
+        return self._TOKEN
 
     @property
     def token_encrypted(self)->str:
-        return self.__class__.MU_TOKEN_CRYPT(self.token)
+        return self.__class__._MU_TOKEN_CRYPT(self.token)
 
     ##
     @property
@@ -182,26 +202,25 @@ class MU_Token():
         MU_Interface.client.token_auth_delete()
 
     def auth(self, token_input:str)->bool:
-        return MU_TokenService.token_auth(self.key, token_input)
+        return MU_TokenService.token_validity(self.key, token_input)
 
 class MU_ORM():
-    USER_UNWRAP = None
-    USER_GET = None
+    _MU_ORM_UNWRAP = None
+    _MU_ORM_GET = None
 
-    def user_unwrap(func)->callable:
-        MU_ORM.USER_UNWRAP = func
+    def user_unwrap(func)->None:
+        MU_ORM._MU_ORM_UNWRAP = func
 
-    def user_get(func)->callable:
-        MU_ORM.USER_GET = func
+    def user_get(func)->None:
+        MU_ORM._MU_ORM_GET = func
 
     @classmethod
     def _user_unwrap(cls, *args, **kwargs):
-        print('user_unwrap: ', cls.USER_UNWRAP)
-        return cls.USER_UNWRAP(*args, **kwargs)
+        return cls._MU_ORM_UNWRAP(*args, **kwargs)
 
     @classmethod
     def _user_get(cls, *args, **kwargs):
-        return cls.USER_GET(*args, **kwargs)
+        return cls._MU_ORM_GET(*args, **kwargs)
 
 class MU_Auth():
     @staticmethod
@@ -214,10 +233,10 @@ class MU_Auth():
 
     @staticmethod
     def logout()->None:
-        token_auth_json = MU_Interface.client.token_auth
+        token_auth_json = MU_Interface.client.token_auth_get()
         token_auth = MU_Token(**token_auth_json)
 
-        token_autn.delete()
+        token_auth.delete()
         MU_Interface.client.user_delete()
 
 class MU_Permission():
@@ -248,13 +267,12 @@ class MU_Permission():
             @wraps(func)
             def wrapper(*args, **kwargs):
                 if MU_Interface.client.logoutted:
-                    MU_Interface.client.token_auth_delete()
                     MU_Interface.client.abort(401)
 
                 user_current = MU_Interface.client.user
-                user = MU_ORM._user_get(**user_current)
+                user_id = user_current.get(MU_Interface.table_id, None)
+                user = MU_ORM._user_get(user_id)
                 if not user.authorized(*permission):
-                    MU_Interface.client_token_auth_delete()
                     MU_Interface.client.abort(403)
 
                 return func(*args, **kwargs)
@@ -264,6 +282,7 @@ class MU_Permission():
 ##
 class ManagerUser(
         MU_TokenService
+        , MU_Token
         , MU_Client
         , MU_Interface
         , MU_ORM
--- 2_year/HappyRotel/app/begin/globals/flask_auth/Role.py
+++ 2_year/HappyRotel/app/begin/globals/flask_auth/Role.py
@@ -1,73 +1,272 @@
+from sqlalchemy import event
+from sqlalchemy.orm import Session, DeclarativeMeta, InstrumentedAttribute
+
+##
 class Permission():
     def __init__(self, tag:str, value:int)->None:
-        self.TAG: str = tag
-        self.VALUE: int = value
+        self._TAG: str = tag
+        self._VALUE: int = value
 
     @property
     def tag(self)->str:
-        return self.TAG
+        return self._TAG
 
     @property
     def value(self)->int:
-        return self.VALUE
+        return self._VALUE
+
+    @property
+    def json(self)->dict[str:int]:
+        return {
+            "tag": self.tag
+            , "value": self.value
+        }
 
 
-class Role():
-    def __init__(self, tags:dict[str:int] | list[str], **kwargs)->None:
-        role = kwargs.get("role", None)
-
-        self.PERMISSIONS:list[Permission] = []
-
-        if not role is None :
-            for tag in tags:
-                self.PERMISSIONS.append(role.permission_get(tag))
+    def update(self, *args, **kwargs)->None:
+        if len(args) == 1 and isinstance(args[0], Permission):
+            self.update_from_permission(*args)
         else:
-            for tag_name, tag_value in tags.items():
-                self.PERMISSIONS.append(Permission(tag_name, tag_value))
+            self.update_from_kwargs(**kwargs)
+
+    def update_from_kwargs(self, **kwargs)->None:
+        self._TAG = kwargs.get("tag", self.tag)
+        self._VALUE = kwargs.get("value", self.value)
+
+    def update_from_permission(self, permission:"Permission")->None:
+        self.update_from_kwargs(**permission.json)
+    
+
+    def delete(self)->None:
+        self._TAG = None
+        self._VALUE = None
+
+    @property
+    def deleted(self)->bool:
+        return self.tag is None and self.value is None
+
+##
+class RolePermissionManager():
+    def __init__(self)->None:
+        self._DICT = {}
+
+
+    def __getitem__(self, key:'T')->Permission:
+        self.permission_check(key)
+        return self.dict[key]
+
+    def __setitem__(self, key:'T', permission:Permission)->None:
+        self.dict[key] = permission
+
+    def __delitem__(self, key:'T')->None:
+        del self.dict[key]
+
+    def __repr__(self)->str:
+        for key in self.keys():
+            self.permission_check(key)
+        return repr(self.dict)
+
+    def __len__(self)->int:
+        return len(self.keys())
+
+
+    def keys(self)->tuple:
+        return tuple(self.dict.keys())
+
+    def items(self)->list[tuple]:
+        return self.dict.items()
+
+    def values(self)->tuple:
+        return tuple(self.dict.values())
+
+
+    def get(self, key:'T', *args)->Permission:
+        self.permission_check(key)
+        return self.dict.get(key, *args)
+
+    ##
+    @property
+    def dict(self)->dict['T':Permission]:
+        return self._DICT
+    
+    def permission_check(self, key:'T')->None:
+        if self.dict[key].deleted:
+            del self.dict[key]
+
+class RoleMixin():
+    @property
+    def tag_permission(self)->dict[str:Permission]:
+        if not "_TAG_PERMISSION" in vars(self):
+            self._TAG_PERMISSION = RolePermissionManager()
+
+        return self._TAG_PERMISSION
+
+    @tag_permission.setter
+    def tag_permission(self, permissions:list[Permission])->None:
+        self._TAG_PERMISSION = RolePermissionManager()
+        for permission in permissions:
+            self._TAG_PERMISSION[permission.tag] = permission
+
+
+    @property
+    def value_permission(self)->dict[int:Permission]:
+        if not "_VALUE_PERMISSION" in vars(self):
+            self._VALUE_PERMISSION = RolePermissionManager()
+
+        return self._VALUE_PERMISSION
+
+    @value_permission.setter
+    def value_permission(self, permissions:list[Permission])->None:
+        self._VALUE_PERMISSION = RolePermissionManager()
+        for  permission in permissions:
+            self._VALUE_PERMISSION[permission.value] = permission
+
+
     ##
     @property
     def tags(self)->dict|None:
-        return [ permission.tag for permission in self.PERMISSIONS ]
+       return [ tag for tag in self.permissions.keys() ]
 
     @property
     def tags_value(self)->int:
-        return [ permission.value for permission in self.PERMISSIONS ]
+        return [ value for value in self.permissions.values() ]
+
 
     @property
     def permissions(self)->dict[str:int]:
         return {
-            permission.tag: permission.value for permission in self.PERMISSIONS
+            permission.tag: permission.value for permission in self.tag_permission.values()
         }
 
-    def permission_get(self, tag:str)->Permission|None:
-        for i in self.PERMISSIONS:
-            if i.tag == tag:
-                return i
+    def permissions_from_tags(self, *tags)->int:
+        return sum([ value for tag, value in self.permissions if tag in tags])
 
-        return None
+    def permission_valid(self, permission:Permission)->bool:
+        if permission.value in self.value_permission:
+            return False
+        return 
+
+    ##
+    def permission_add(self, permission:Permission)->None:
+        if permission.value in self.value_permission.keys():
+            return
+
+        self.tag_permission[permission.tag] = permission
+        self.value_permission[permission.value] = permission
+
+    def permission_del(self, permission:Permission)->None:
+        del self.tag_permission[permission.tag]
+        del self.value_permission[permission.value]
+
+    def permission_get(self, tag:str)->Permission|None:
+        return self.tag_permission.get(tag, None)
+
+    def permission_swap(self, tag:str, permission_swap:Permission)->None:
+        permission = self.permission_get(tag)
+        self.permission_del(permission)
+
+        permission.update(permission_swap)
+        self._TAG_PERMISSION[permission.tag] = permission
 
     def permission_value_get(self, tag:str)->int|None:
         permission: Permission = self.permission_get(tag)
         return permission.value if permission else None
 
-
-    def authorized_by_role(self, role:"Role")->bool:
+    ##
+    def authorized_by_role(self, role:"RoleMixin")->bool:
         return (sum(role.tags_value) & sum(self.tags_value)) == sum(role.tags_value)
     
     def authorized_by_int(self, permission:int)->bool:
         return (permission & sum(self.tags_value)) == permission
 
-    def authorized_by_tags(self, *tags:list[str])->bool:
-        return not False in [ tag in self.tags for tag in tags ]
+
+class Role(RoleMixin):
+    def __init__(self, permissions:list[str] | dict[str:int] , role:RoleMixin=None)->None:
+        if role is None:
+            self.init_from_tags(permissions)
+        else:
+            self.init_from_role(role, permissions)
+
+    def init_from_tags(self, permissions:dict[str:int])->None:
+        for tag, value in permissions.items():
+            self.permission_add(Permission(tag, value))
+
+    def init_from_role(self, role:RoleMixin, permissions:list[str])->None:
+        for tag in permissions:
+            permission = role.permission_get(tag)
+            self.permission_add(permission)
 
 ##
-"""
-roleAdmin = Role([
-    Permission("MANAGE_USER", 1)
-    , Permission("MANAGE_ROOM", 2)
-    , Permission("MANAGE_INVOICE", 4)
-    , Permission("MANAGE_RESERVE", 8)
-])
+class Role_ORM():
+    _TABLE_COLUMN_TAG_NAME = 'tag'
+    _TABLE_COLUMN_VALUE_NAME = 'value'
+
+    def __init__(self, table:DeclarativeMeta, **kwargs)->None:
+        self._TABLE = table
+        self._TABLE_COLUMN_TAG_NAME = kwargs.get("column_tag_name") or self._TABLE_COLUMN_TAG_NAME
+        self._TABLE_COLUMN_VALUE_NAME = kwargs.get("column_value_name") or self._TABLE_COLUMN_VALUE_NAME
+
+        if None in [ self.column_tag, self.column_value ]:
+            raise AttributeError(f"Plase, ensure that the columns {self.column_tag_name} and {self.column_value_name} exists in table {self.table.__tablename}")
+
+        ##
+        for table_i in self.session.query(self.table):
+            self.permission_add_from_orm(table_i)
+
+        event.listen(self.table, "after_insert", self.permission_insert)
+        event.listen(self.table, "before_delete", self.permission_delete)
+        
+    ##
+    def permission_add_from_orm(self, table_i:DeclarativeMeta)->None:
+        tag, value = getattr(table_i, self.column_tag_name, None), getattr(table_i, self.column_value_name, None)
+        self.permission_add(Permission(tag, value))
+
+    def permission_del_from_orm(self, table_i:DeclarativeMeta)->None:
+        tag = getattr(table_id, self.column_tag_name, None)
+        self.permission_del(tag)
+    
+    ##
+    def permission_insert(self, mapper, connection, instance)->None:
+        self.permission_add_from_orm(instance)
+
+    def permission_delete(self, mapper, connection, instance)->None:
+        self.permission_del_from_orm(instance)
+
+    ##
+    @property
+    def table(self)->DeclarativeMeta:
+        return self._TABLE
+
+    
+    @property
+    def column_tag_name(self)->str:
+        return self._TABLE_COLUMN_TAG_NAME
+
+    @property
+    def column_value_name(self)->str:
+        return self._TABLE_COLUMN_VALUE_NAME
+
+
+    @property
+    def column_tag(self)->InstrumentedAttribute|None:
+        return getattr(self.table, self.column_tag_name, None)
+
+    @property
+    def column_value(self)->InstrumentedAttribute|None:
+        return getattr(self.table, self.column_value_name, None)
+
+
+    @property
+    def session(self)->Session:
+        return self._SESSION
+
+##
+roleAdmin = Role({
+    "MANAGE_USER": 1
+    , "MANAGE_ROOM": 2
+    , "MANAGE_INVOICE": 4
+    , "MANAGE_RESERVE": 8
+})
 
 roleEmployer = Role([
     "MANAGE_INVOICE"
@@ -75,14 +274,15 @@ roleEmployer = Role([
 ], role=roleAdmin
 )
 
+"""
 print('tags: ',roleEmployer.tags)
 print('arole: ', roleEmployer.authorized_by_role(roleAdmin))
-print('aint: ', roleEmployer.authorized_by_int(2))
-print('atags: ', roleEmployer.authorized_by_tags("MANAGE_INVOICE", "MANAGE_RESERVE", "MANAGE_USER"))
+print('aint: ', roleEmployer.authorized_by_int(11))
 
 print()
 print('tags: ', roleAdmin.tags)
 print('aroles: ', roleAdmin.authorized_by_role(roleEmployer))
 print('aint: ', roleAdmin.authorized_by_int(1))
-print('atags: ', roleAdmin.authorized_by_tags("MANAGE_INVOICE", "MANAGE_RESERVE", "MANAGE_USER"))
 """
+roleAdmin.permission_swap("MANAGE_INVOICE", Permission('GERENCIAR_FATURA', 1))
+print(roleEmployer.permissions.keys())
